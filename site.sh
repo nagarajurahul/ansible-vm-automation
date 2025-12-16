4ff1df8a392a033e0df12d956ba3161a87ce86b0
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-12-16 00:53:24 -0600

    add: monitoring role. playbook and update script to run it

--- site.sh
+++ site.sh
@@ -38,6 +38,10 @@ print_banner "Install secrets prior to deploying ArgoCD application and GoFlight
 ansible-playbook -i k8s.ini playbook-secrets.yml
 sleep $PAUSE_BETWEEN
 
+print_banner "Installing Monitoring Stack"
+ansible-playbook -i k8s.ini playbook-monitoring.yml
+sleep $PAUSE_BETWEEN
+
 print_banner "Installing Cert-Manager"
 ansible-playbook -i k8s.ini playbook-cert-manager.yml
 sleep $PAUSE_BETWEEN
